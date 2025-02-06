06255d1890a41e5e482bfc35eab0fce1b80ce86f
Mishra Ravi Shivshankar <132126056+RAMRAMRAMR@users.noreply.github.com>
2025-02-06 12:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	let sp=s.split(" ");
+	return sp[0];
 }
+
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
